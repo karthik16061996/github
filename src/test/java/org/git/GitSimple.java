d5903b16f9944ec772d714a95b6ad03181641db0
karthik16061996 <karthiknaik005@gmail.com>
2023-03-05 21:41:38 +0530

    updated

--- src/test/java/org/git/GitSimple.java
+++ src/test/java/org/git/GitSimple.java
@@ -13,7 +13,22 @@ public class GitSimple {
 	}
 
 	private void method3() {
-		System.out.println("2");
+		System.out.println("3");
+
+	}
+
+	private void method7() {
+		System.out.println("7");
+
+	}
+
+	private void method8() {
+		System.out.println("7");
+
+	}
+
+	private void method9() {
+		System.out.println("7");
 
 	}
 
